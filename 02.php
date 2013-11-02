2f985cf8bc1430fb1de979538e7c503a3fc7c6ca
Jan Dolecek <juzna.cz@gmail.com>
2013-11-02 15:11:06 +0100

    flow selenium

---
+++ 02.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Controlling selenium with promises
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+// React services
+$eventLoop = React\EventLoop\Factory::create();
+
+$dnsFactory = new React\Dns\Resolver\Factory();
+$dnsResolver = $dnsFactory->createCached("8.8.8.8", $eventLoop);
+
+$httpClientFactory = new React\HttpClient\Factory();
+$httpClient = $httpClientFactory->create($eventLoop, $dnsResolver);
+
+Flow\Flow::$eventLoop = $eventLoop;
+
+
+flow(function() {
+	$wd = new WebDriver\WebDriver("http://127.0.0.1:4444/wd/hub");
+	$session = (yield $wd->session());
+	yield $session->open('http://localhost/php-webdriver/page.php');
+
+	$el = (yield $session->element('class name', 'seznam'));
+	yield $el->click();
+
+	echo "Done\n";
+});
+
+
+echo "flow finished\n";
